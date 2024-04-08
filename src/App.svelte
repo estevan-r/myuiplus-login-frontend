6ea5bf673f57c979e48140710db08dfc21e94b88
estevan-r <ruiz.estevan@gmail.com>
2024-04-08 14:48:49 -0600

    Add navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,8 @@
 <script>
   import "./app.css";
+  import NavBar from "./lib/Nav.svelte";
 </script>
+
+<main class="mx-9 mt-8">
+  <NavBar />
+</main>
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,31 @@
+<script>
+  import logo from "../assets/cdle-logo.svg";
+</script>
+
+<header>
+  <nav class="navbar">
+    <div class="flex-1">
+      <a href="https://coloradoui.gov/">
+        <img src={logo} alt="CDLE logo" class="w-[200px] h-auto" />
+      </a>
+    </div>
+    <ul class="menu menu-horizontal px-1 gap-8">
+      <li>
+        <a href="/">Find a Job</a>
+      </li>
+      <li>
+        <a href="/">Report Fraud</a>
+      </li>
+      <li>
+        <a href="/">Claimant Guide</a>
+      </li>
+      <li>
+        <a href="/">Maintaining Eligibility</a>
+      </li>
+      <li>
+        <a href="/">Assistance Programs</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="w-full h-[1px] bg-base-200 my-4"></div>
+</header>
