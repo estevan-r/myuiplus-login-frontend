064240854bf5f200cb68ea5522077182d4964f39
estevan-r <ruiz.estevan@gmail.com>
2024-04-08 15:18:37 -0600

    Add alert component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
   import "./app.css";
   import NavBar from "./lib/Nav.svelte";
+  import Alerts from "./lib/Alerts.svelte";
 </script>
 
 <main class="mx-9 mt-8">
   <NavBar />
+  <Alerts />
 </main>
---
+++ src/lib/Alerts.svelte
@@ -0,0 +1,52 @@
+<!-- Refactor to call text as data -->
+<div class="alert alert-info p-6 items-start shadow">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    class="stroke-current shrink-0 w-6 h-6 text-base-content"
+    ><path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    ></path></svg
+  >
+  <ul class="text-base-content flex flex-col gap-3 px-6">
+    <li>
+      <p>
+        A claim must be fully processed, and payments are held until all pending
+        issues are resolved. <strong
+          >While issues are pending or during appeals, you must request payment
+          each week to avoid your claim closing.</strong
+        > The amount of time claims take to process varies and may take up to 5 to
+        7 weeks from the file date.
+      </p>
+    </li>
+    <li>
+      <p>
+        If you have a Program Integrity Issue on your claim, please ensure you
+        upload a copy of your Driver's License or ID, a picture of yourself, a
+        picture of your Social Security Card or Birth Certificate, and a current
+        bill reflecting your current address matching your claim information at
+        a minimum. Additional documents may be requested to resolve this issue.
+      </p>
+    </li>
+    <li>
+      <p>
+        If you do not provide documents requested by us, your claim may be
+        denied. Uploads can be made directly into the issue in MyUI+ or on our
+        website under the "Upload Documents" section at <a
+          href="https://coloradoui.gov/"
+          class="link">ColoradoUI.gov</a
+        >.
+      </p>
+    </li>
+  </ul>
+</div>
+
+<style>
+  ul {
+    list-style-type: disc;
+  }
+</style>
