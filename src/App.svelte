151d4ffb81a15a0c62e9461544f2978adbe4cf49
estevan-r <ruiz.estevan@gmail.com>
2024-04-18 16:31:11 -0600

    Refactor nav

--- src/App.svelte
+++ src/App.svelte
@@ -1,61 +1,18 @@
 <script>
   import "./app.css";
-  import NavBar from "./lib/Nav.svelte";
+  import Nav from "./lib/Nav.svelte";
   import Alerts from "./lib/Alerts.svelte";
   import HeroCard from "./lib/HeroCard.svelte";
   import SecondaryCard from "./lib/SecondaryCard.svelte";
   import TertiaryCards from "./lib/TertiaryCards.svelte";
   import Footer from "./lib/Footer.svelte";
-  import logo from "./assets/cdle-logo.svg";
-  import emblem from "./assets/cdle-emblem.svg";
 </script>
 
 <div class="drawer">
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
   <div class="drawer-content flex flex-col">
-    <!-- Navbar -->
-    <div
-      class="w-full sm:px-6 md:px-9 2xl:px-0 max-w-[1428px] m-auto navbar lg:py-4 bg-base-100 border-b border-base-200"
-    >
-      <div class="flex-1 px-2">
-        <a href="https://coloradoui.gov/">
-          <img
-            src={logo}
-            alt="CDLE logo"
-            class="md:block hidden w-[200px] h-auto"
-          />
-          <img src={emblem} alt="CDLE emblem" class="md:hidden w-auto h-9" />
-        </a>
-      </div>
-      <div class="flex-none lg:hidden">
-        <label
-          for="my-drawer-3"
-          aria-label="open sidebar"
-          class="btn btn-active btn-ghost"
-        >
-          Menu
-        </label>
-      </div>
+    <Nav />
 
-      <div class="flex-none hidden lg:block">
-        <ul class="menu menu-horizontal gap-6">
-          <!-- Navbar menu content here -->
-          <li>
-            <a
-              href="https://cdle.colorado.gov/unemployment/ui-claimant-guide"
-              target="_blank">Claimant Guide</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://cdle.colorado.gov/unemployment/assistance-programs"
-              target="_blank">Assistance Programs</a
-            >
-          </li>
-        </ul>
-      </div>
-    </div>
-    <!-- Page content here -->
     <main class="max-w-[1500px] m-auto px-2 sm:px-6 md:px-9 pt-4">
       <Alerts />
       <HeroCard />
@@ -69,7 +26,6 @@
     <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"
     ></label>
     <ul class="menu p-6 gap-6 sm:w-80 w-72 min-h-full bg-base-200">
-      <!-- Sidebar content here -->
       <li>
         <a
           href="https://cdle.colorado.gov/unemployment/ui-claimant-guide"
--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,36 +3,43 @@
   import emblem from "../assets/cdle-emblem.svg";
 </script>
 
-<header>
-  <nav class="navbar justify-between">
-    <div>
-      <a href="https://coloradoui.gov/">
-        <img
-          src={logo}
-          alt="CDLE logo"
-          class="md:block hidden w-[200px] h-auto"
-        />
-        <img src={emblem} alt="CDLE emblem" class="md:hidden w-auto h-9" />
-      </a>
-    </div>
+<div
+  class="w-full sm:px-6 md:px-9 2xl:px-0 max-w-[1428px] m-auto navbar lg:py-4 bg-base-100 border-b border-base-200"
+>
+  <div class="flex-1 px-2">
+    <a href="https://coloradoui.gov/">
+      <img
+        src={logo}
+        alt="CDLE logo"
+        class="md:block hidden w-[200px] h-auto"
+      />
+      <img src={emblem} alt="CDLE emblem" class="md:hidden w-auto h-9" />
+    </a>
+  </div>
+  <div class="flex-none lg:hidden">
+    <label
+      for="my-drawer-3"
+      aria-label="open sidebar"
+      class="btn btn-active btn-ghost"
+    >
+      Menu
+    </label>
+  </div>
 
-    <ul class="hidden menu menu-horizontal px-1 gap-8">
+  <div class="flex-none hidden lg:block">
+    <ul class="menu menu-horizontal gap-6">
       <li>
-        <a href="/">Find a Job</a>
+        <a
+          href="https://cdle.colorado.gov/unemployment/ui-claimant-guide"
+          target="_blank">Claimant Guide</a
+        >
       </li>
       <li>
-        <a href="/">Report Fraud</a>
-      </li>
-      <li>
-        <a href="/">Claimant Guide</a>
-      </li>
-      <li>
-        <a href="/">Maintaining Eligibility</a>
-      </li>
-      <li>
-        <a href="/">Assistance Programs</a>
+        <a
+          href="https://cdle.colorado.gov/unemployment/assistance-programs"
+          target="_blank">Assistance Programs</a
+        >
       </li>
     </ul>
-  </nav>
-  <div class="w-full h-[1px] bg-base-200 my-3"></div>
-</header>
+  </div>
+</div>
